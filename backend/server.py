61378d34e8391c33c82aefd812fe6a6ffe295e89
CoCo9122 <banaji.yu0517@outlook.jp>
2022-06-21 00:04:31 +0900

    socket.io docker

--- backend/server.py
+++ backend/server.py
@@ -23,4 +23,4 @@ def test_disconnect(data):
     
 
 if __name__ == "__main__":
-    socket_io.run(app, host='0.0.0.0', port=5000)
+    socket_io.run(app, host='0.0.0.0', port=80)
--- frontend/src/context/context-socketio.js
+++ frontend/src/context/context-socketio.js
@@ -1,14 +1,18 @@
 import { createContext, useState, useEffect } from 'react'
-import client from "socket.io-client"
+import { Manager } from "socket.io-client"
 
 const Context = createContext({})
 
 export function ContextSocketProvider({children}) {
     const [Socket, setSocket] = useState(null)
     useEffect(() => {
-        const SOCKET_URL = "ws://localhost:5000"
-        const socket = client(SOCKET_URL)
+        const SOCKET_URL = "ws://localhost:5001"
+        const manager = new Manager(SOCKET_URL)
+        const socket = manager.socket('/')
         setSocket(socket)
+        Socket.on("connect", () => {
+            console.log('socket.connectを出力')
+        })
     }, [])
     return <Context.Provider value={{Socket}}>{children}</Context.Provider>
 }
